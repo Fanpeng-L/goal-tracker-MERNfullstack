0980dca5850f7cc2622eb955fea78d3f82e016f6
Fanpeng Li <serforfp@gmail.com>
2023-12-27 21:26:14 +0800

    Add errorMiddleware to override express error handler

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler); // override express default error handler
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
