4bdc3952f341dba42edaa9ec34d15a8b23df6f88
Fanpeng Li <serforfp@gmail.com>
2024-01-26 11:01:13 +0000

    Create userRoutes and userController

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,17 +2,17 @@ const asyncHandler = require("express-async-handler");
 
 const Goal = require("../models/goalModel");
 
-// @desc Get goals
-// @route GET /api/goals
-// @access Private
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
 const getGoals = asyncHandler(async (req, res) => {
   const goals = await Goal.find();
   res.status(200).json(goals);
 });
 
-// @desc Set goals
-// @route POST /api/goals
-// @access Private
+// @desc    Set goals
+// @route   POST /api/goals
+// @access  Private
 const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
@@ -25,9 +25,9 @@ const setGoal = asyncHandler(async (req, res) => {
   res.status(200).json(goal);
 });
 
-// @desc Update goals
-// @route PUT /api/goals/:id
-// @access Private
+// @desc    Update goals
+// @route   PUT /api/goals/:id
+// @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
 
