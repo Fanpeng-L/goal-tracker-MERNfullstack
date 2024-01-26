13b59fa0a13d52b4e5063384c77ba2e0f7420752
Fanpeng Li <serforfp@gmail.com>
2024-01-26 17:18:54 +0000

    Authenticate user login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,15 +44,26 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Invalid user data");
   }
-
-  res.json({ message: "Register user" });
 });
 
 // @desc     Authenticate a user
 // @route    POST /api/users/login
 // @access   Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login user" });
+  const { email, password } = req.body;
+  // check for user email
+  const user = await User.findOne({ email });
+  // compare the password with hashed password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credential");
+  }
 });
 
 // @desc     Get user data
