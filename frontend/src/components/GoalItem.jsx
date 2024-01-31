1e1a849bbfc3ada155b5c35c02875a1b5437cd5b
Fanpeng Li <serforfp@gmail.com>
2024-01-31 23:15:53 +0000

    Finished Delete goals function

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,8 +1,17 @@
+import { FiDelete } from "react-icons/fi";
+import { useDispatch } from "react-redux";
+import { deleteGoal } from "../features/goals/goalSlice";
+
 function GoalItem({ goal }) {
+  const dispatch = useDispatch();
+
   return (
     <div className="goal">
       <div>{new Date(goal.createdAt).toLocaleString("en-US")}</div>
       <h2>{goal.text}</h2>
+      <button className="close" onClick={() => dispatch(deleteGoal(goal._id))}>
+        <FiDelete />
+      </button>
     </div>
   );
 }
