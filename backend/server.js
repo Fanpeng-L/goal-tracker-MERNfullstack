de3af14c9468d128cf0ba0b32752c12e1eeae356
Fanpeng Li <serforfp@gmail.com>
2024-02-01 15:59:37 +0000

    Added cors package

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
@@ -12,6 +13,7 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 // routes
 app.use("/api/goals", require("./routes/goalRoutes"));
